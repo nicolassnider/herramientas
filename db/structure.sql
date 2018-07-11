5ea4c486f60f841336626b5ee1acd243ae565290
Nicolás Snider <nsnider@soulitsolutions.com>
2018-07-11 10:13:53 -0300

    commit varios

--- db/structure.sql
+++ db/structure.sql
@@ -1,348 +1,80 @@
--- phpMyAdmin SQL Dump
--- version 4.8.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 09-07-2018 a las 17:12:26
--- Versión del servidor: 10.1.33-MariaDB
--- Versión de PHP: 7.2.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `herramientas`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `campanias`
---
-
-CREATE TABLE `campanias` (
-  `id` bigint(4) NOT NULL,
-  `numero_campania` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `fecha_inicio` date DEFAULT NULL,
-  `fecha_cierre` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `catalogo`
---
-
-CREATE TABLE `catalogo` (
-  `id` bigint(4) NOT NULL,
-  `campania` varchar(45) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categoria`
---
-
-CREATE TABLE `categoria` (
-  `id` bigint(4) NOT NULL,
-  `nombre` varchar(45) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `clientes`
---
-
-CREATE TABLE `clientes` (
-  `id` bigint(4) NOT NULL,
-  `persona` bigint(4) NOT NULL,
-  `apodo` varchar(15) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `anio_nacimiento` year(4) DEFAULT NULL,
-  `es_madre` bit(1) DEFAULT b'0',
-  `direccion_entrega` varchar(45) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pedidos`
---
-
-CREATE TABLE `pedidos` (
-  `id` bigint(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pedido_cliente`
---
-
-CREATE TABLE `pedido_cliente` (
-  `id` bigint(4) NOT NULL,
-  `fecha` date NOT NULL,
-  `cliente` int(11) NOT NULL,
-  `total` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pedido_cliente_producto`
---
-
-CREATE TABLE `pedido_cliente_producto` (
-  `id` bigint(4) NOT NULL,
-  `pedido_cliente` bigint(4) NOT NULL,
-  `producto` bigint(4) NOT NULL,
-  `cantidad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `personas`
---
-
-CREATE TABLE `personas` (
-  `id` bigint(4) NOT NULL,
-  `documento_tipo` bigint(4) NOT NULL,
-  `documento_numero` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
-  `nombre` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `apellido` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `email` varchar(20) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `telefono` varchar(11) COLLATE utf8_spanish2_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `producto`
---
-
-CREATE TABLE `producto` (
-  `id` bigint(4) NOT NULL,
-  `nombre` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
-  `categoria` bigint(4) DEFAULT NULL,
-  `campania` bigint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `revendedoras`
---
-
-CREATE TABLE `revendedoras` (
-  `id` bigint(4) NOT NULL,
-  `persona` bigint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipo_documento`
---
-
-CREATE TABLE `tipo_documento` (
-  `id` bigint(3) NOT NULL,
-  `descripcion` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `tipo_documento`
---
-
-INSERT INTO `tipo_documento` (`id`, `descripcion`) VALUES
-(1, 'DU'),
-(2, 'PASAPORTE');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `campanias`
---
-ALTER TABLE `campanias`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `catalogo`
---
-ALTER TABLE `catalogo`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `pedidos`
---
-ALTER TABLE `pedidos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `pedido_cliente`
---
-ALTER TABLE `pedido_cliente`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `pedido_cliente_producto`
---
-ALTER TABLE `pedido_cliente_producto`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `personas`
---
-ALTER TABLE `personas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `revendedoras`
---
-ALTER TABLE `revendedoras`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tipo_documento`
---
-ALTER TABLE `tipo_documento`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `campanias`
---
-ALTER TABLE `campanias`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `catalogo`
---
-ALTER TABLE `catalogo`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `clientes`
---
-ALTER TABLE `clientes`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `pedidos`
---
-ALTER TABLE `pedidos`
-  MODIFY `id` bigint(7) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `pedido_cliente`
---
-ALTER TABLE `pedido_cliente`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `pedido_cliente_producto`
---
-ALTER TABLE `pedido_cliente_producto`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `personas`
---
-ALTER TABLE `personas`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `producto`
---
-ALTER TABLE `producto`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `revendedoras`
---
-ALTER TABLE `revendedoras`
-  MODIFY `id` bigint(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `tipo_documento`
---
-ALTER TABLE `tipo_documento`
-  MODIFY `id` bigint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `clientes`
---
-ALTER TABLE `clientes`
-  ADD CONSTRAINT `persona_fk_cliente` FOREIGN KEY (`id`) REFERENCES `personas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `pedido_cliente_producto`
---
-ALTER TABLE `pedido_cliente_producto`
-  ADD CONSTRAINT `producto` FOREIGN KEY (`id`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `personas`
---
-ALTER TABLE `personas`
-  ADD CONSTRAINT `documento_tipo` FOREIGN KEY (`id`) REFERENCES `tipo_documento` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD CONSTRAINT `campania` FOREIGN KEY (`id`) REFERENCES `campanias` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `categoria` FOREIGN KEY (`id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `revendedoras`
---
-ALTER TABLE `revendedoras`
-  ADD CONSTRAINT `persona` FOREIGN KEY (`id`) REFERENCES `personas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--1.0.0
+--Creación de tablas
+CREATE TABLE `herramientas`.`campania` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_campania`));
+
+CREATE TABLE `herramientas`.`categoria_producto` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_categoria_producto`));
+
+CREATE TABLE `herramientas`.`unidad_producto` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_unidad_producto`));
+
+CREATE TABLE `herramientas`.`cliente` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_cliente`));
+
+CREATE TABLE `herramientas`.`catalogo` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_catalogo`));
+
+CREATE TABLE `herramientas`.`producto` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_producto`));
+
+CREATE TABLE `herramientas`.`pedido_cliente` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_pedido_cliente`));
+
+CREATE TABLE `herramientas`.`pedido_campania` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_pedido_campania`));
+
+CREATE TABLE `herramientas`.`remito_producto` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_remito_producto`));
+
+CREATE TABLE `herramientas`.`producto_catalogo` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_producto_catalogo`));
+
+CREATE TABLE `herramientas`.`pedido_catalogo` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_pedido_catalogo`));
+
+CREATE TABLE `herramientas`.`factura` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_factura`));
+
+CREATE TABLE `herramientas`.`remito` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_remito`));
+
+CREATE TABLE `herramientas`.`persona` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_persona`));
+
+CREATE TABLE `herramientas`.`tipo_documento` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_tipo_documento`));
+
+CREATE TABLE `herramientas`.`revendedora` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_revendedora`));
+
+CREATE TABLE `herramientas`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_usuario`));
+
+CREATE TABLE `herramientas`.`categoria_revendedora` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_categoria_revendedora`));
+
+CREATE TABLE `herramientas`.`pedido_revendedora` (
+  `id` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_pedido_revendedora`));
+
+
+
