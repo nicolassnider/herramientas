ed5027fa292c3ed13957abcca102a09d48429886
Nicolas Snider <nsnider@soulitsolutions.com>
2018-07-12 00:06:56 -0300

    commit estructura

--- db/structure.sql
+++ db/structure.sql
@@ -3,7 +3,7 @@
 CREATE SCHEMA `herramientas` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci;
 
 #Creación de tablas
-
+#1
 CREATE TABLE `herramientas`.`campania` (
     `id_campania` INT NOT NULL AUTO_INCREMENT,
     `fecha_inicio` DATE NOT NULL,
@@ -11,17 +11,34 @@ CREATE TABLE `herramientas`.`campania` (
     `numero_campania` VARCHAR (20) NULL,
     `activo` BIT(1) NULL,
     PRIMARY KEY (`id_campania`));
-
+#2
+CREATE TABLE `herramientas`.`catalogo` (
+    `id_catalogo` INT NOT NULL AUTO_INCREMENT,
+    `descripcion` VARCHAR(30) NOT NULL,
+    `observaciones` TINYTEXT NULL,
+    `activo` BIT(1) NULL,
+    PRIMARY KEY (`id_catalogo`));
+#3
 CREATE TABLE `herramientas`.`categoria_producto` (
     `id_categoria_producto` INT NOT NULL AUTO_INCREMENT,
     `descripcion` VARCHAR(30) NOT NULL,
     PRIMARY KEY (`id_categoria_producto`));
-
+#4
 CREATE TABLE `herramientas`.`unidad_producto` (
     `id_unidad_producto` INT NOT NULL AUTO_INCREMENT,
     `descripcion` VARCHAR(30) NOT NULL,
   PRIMARY KEY (`id_unidad_producto`));
-
+#5
+CREATE TABLE `herramientas`.`tipo_documento` (
+    `id_tipo_documento` INT NOT NULL AUTO_INCREMENT,
+    `descripcion` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id_tipo_documento`));
+#6
+CREATE TABLE `herramientas`.`categoria_cliente` (
+    `id_categoria_cliente` INT NOT NULL AUTO_INCREMENT,
+    `descripcion` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id_categoria_cliente`));
+$7
 CREATE TABLE `herramientas`.`cliente` (
     `id_cliente` INT NOT NULL AUTO_INCREMENT,
     `direccion_entrega` VARCHAR(30) NOT NULL,
@@ -31,96 +48,81 @@ CREATE TABLE `herramientas`.`cliente` (
     `es_madre` BIT(1) NULL,
     `persona` INT NOT NULL,
     `activo` BIT(1) NULL,
-  PRIMARY KEY (`id_cliente`));
-  
-CREATE TABLE `herramientas`.`ubicacion` (
-    `id_ubicacion` INT NOT NULL AUTO_INCREMENT,
-    `ubicacion` VARCHAR(30) NOT NULL,
-    PRIMARY KEY (`id_ubicacion`));
-
-CREATE TABLE `herramientas`.`catalogo` (
-    `id_catalogo` INT NOT NULL AUTO_INCREMENT,
-    `descripcion` VARCHAR(30) NOT NULL,
-    `observaciones` TINYTEXT NULL,
-    `activo` BIT(1) NULL,
-    PRIMARY KEY (`id_catalogo`));
-  
+    PRIMARY KEY (`id_cliente`));
+#8
+CREATE TABLE `herramientas`.`revendedora` (
+  `id_revendedora` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_revendedora`));
+#9
 CREATE TABLE `herramientas`.`catalogo_campania` (
     `id_catalogo_campania` INT NOT NULL AUTO_INCREMENT,
     `catalogo` INT NOT NULL,
     `campania` INT NOT NULL,
     `activo` BIT(1) NULL,
-    PRIMARY KEY (`id_catalogo`));
-
+    PRIMARY KEY (`id_catalogo_campania`));
+#10
 CREATE TABLE `herramientas`.`producto` (
     `id_producto` INT NOT NULL AUTO_INCREMENT,
     `descripcion` VARCHAR(30) NOT NULL,
-    `categoria_producto` INT NOT NULL,
+    `categoria` INT NOT NULL,
     `unidad` INT NOT NULL,
   PRIMARY KEY (`id_producto`));
-
+#11
+CREATE TABLE `herramientas`.`producto_catalogo`(
+    `id_producto_catalogo` INT NOT NULL AUTO_INCREMENT,
+    `producto` INT NOT NULL,
+    `catalogo` INT NOT NULL,
+    `activo` BIT(1) NULL,
+    PRIMARY KEY(`id_producto_catalogo`));
+#12
 CREATE TABLE `herramientas`.`pedido_cliente` (
     `id_pedido_cliente` INT NOT NULL AUTO_INCREMENT,
+    `cliente` INT NOT NULL,
     `fecha_alta` DATE NOT NULL,
     `entregado` BIT(1) NOT NULL,
     `cobrado` BIT(1) NOT NULL,
     PRIMARY KEY (`id_pedido_cliente`));
-
-CREATE TABLE `herramientas`.`pedido_cliente_producto_catalogo`
-
-CREATE TABLE `herramientas`.`pedido_campania` (
-    `id_pedido_campania` INT NOT NULL AUTO_INCREMENT,
-    `numero_pedido` INT NOT NULL,
-    `campania` INT NOT NULL,
-    `fecha_alta` DATE NOT NULL,
-    `fecha_cierre` DATE NOT NULL,
-    `activo` BIT(1) NULL,
-    PRIMARY KEY (`id_pedido_campania`));
-
-CREATE TABLE `herramientas`.`pedido_campania_producto`(
-
-	)
-CREATE TABLE `herramientas`.`remito_producto` (
-    `id_remito_producto` INT NOT NULL AUTO_INCREMENT,
-
-
-    PRIMARY KEY (`id_remito_producto`));
-
-CREATE TABLE `herramientas`.`producto_catalogo` (
-    `id_producto_catalogo` INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id_producto_catalogo`));
-
-CREATE TABLE `herramientas`.`pedido_catalogo` (
-    `id_pedido_catalogo` INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id_pedido_catalogo`));
-
+#13
+CREATE TABLE `herramientas`.`categoria_revendedora` (
+  `id_categoria_revendedora` INT NOT NULL AUTO_INCREMENT,  
+  PRIMARY KEY (`id_categoria_revendedora`));
+#14
+CREATE TABLE `herramientas`.`pedido_cliente_producto_catalogo`(
+    `id_pedido_cliente_producto_catalogo` INT NOT NULL AUTO_INCREMENT,
+    `pedido_cliente` INT NOT NULL,
+    `producto_catalogo` INT NOT NULL,
+    `cantidad` INT NOT NULL,
+     PRIMARY KEY (`id_pedido_cliente_producto_catalogo`)); 
+#15
 CREATE TABLE `herramientas`.`factura` (
     `id_factura` INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`id_factura`));
-
+#16
 CREATE TABLE `herramientas`.`remito` (
     `id` INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`id_remito`));
-
+#17
+CREATE TABLE `herramientas`.`remito_producto` (
+    `id_remito_producto` INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`id_remito_producto`));
+#18
+CREATE TABLE `herramientas`.`ubicacion` (
+    `id_ubicacion` INT NOT NULL AUTO_INCREMENT,
+    `ubicacion` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`id_ubicacion`));
+#19
 CREATE TABLE `herramientas`.`persona` (
     `id_persona` INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`id_persona`));
 
-CREATE TABLE `herramientas`.`tipo_documento` (
-    `id_tipo_documento` INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id_tipo_documento`));
 
-CREATE TABLE `herramientas`.`revendedora` (
-  `id_revendedora` INT NOT NULL AUTO_INCREMENT,  
-  PRIMARY KEY (`id_revendedora`));
+
 
 CREATE TABLE `herramientas`.`usuario` (
   `id_usuario` INT NOT NULL AUTO_INCREMENT,  
   PRIMARY KEY (`id_usuario`));
 
-CREATE TABLE `herramientas`.`categoria_revendedora` (
-  `id_categoria_revendedora` INT NOT NULL AUTO_INCREMENT,  
-  PRIMARY KEY (`id_categoria_revendedora`));
+
 
 CREATE TABLE `herramientas`.`pedido_revendedora` (
   `id_pedido_revendedora` INT NOT NULL AUTO_INCREMENT,  
