04fd90d0bf15d8423e0756de4ce1c336b73b746d
Nicolas Snider <nsnider@soulitsolutions.com>
2018-08-15 23:55:36 -0300

    commit

--- herramientas_api/Repository/PersonaRepository.php
+++ herramientas_api/Repository/PersonaRepository.php
@@ -15,21 +15,53 @@ class PersonaRepository extends AbstractRepository
     {
 
         try {
+            $tipoDocumento=$persona->getTipoDocumento();
+            $documento=$persona->getDocumento();
+            $telefono=$persona->getTelefono();
+            $email=$persona->getEmail();
+            $activo=$persona->getActivo();
+            $localidad=$persona->getLocalidad();
+            $fechaAltaPersona=date('Y-m-d H:i:s');
 
-            $consulta = "INSERT INTO personas(/*..*/) 
-                        VALUES (/*..*/)";
+            $sqlInsert = "INSERT INTO persona(
+                  tipo_documento, 
+                  documento, 
+                  telefono, 
+                  email, 
+                  activo, 
+                  localidad, 
+                  fecha_alta_persona) 
+                        VALUES (
+                  :tipo_documento, 
+                  :documento, 
+                  :telefono, 
+                  :email, 
+                  :activo, 
+                  :localidad, 
+                  :fecha_alta_persona
+                        )";
 
 
             //prepara inserción base de datos
             $db = $this->connect();
             $db->beginTransaction();
-            $stmt = $db->prepare($consulta);
+            $stmtInsert = $db->prepare($sqlInsert);
+            $stmtInsert->bindParam(':tipo_documento', $tipoDocumento,PDO::PARAM_INT);
+            $stmtInsert->bindParam(':documento',$documento);
+            $stmtInsert->bindParam(':telefono', $telefono);
+            $stmtInsert->bindParam(':email', $email);
+            $stmtInsert->bindParam(':activo', $activo);
+            $stmtInsert->bindParam(':localidad', $localidad,PDO::PARAM_INT);
+            $stmtInsert->bindParam(':fecha_alta_persona', $fechaAltaPersona);
+            $stmtInsert->execute();
+            $persona->setId($db->lastInsertId());
             $db->commit();
+
 
 
         } catch (Exception $e) {
             if ($db != null) $db->rollback();
-            if ($e instanceof PDOException && $stmt->errorInfo()[0] == 23000 && $stmt->errorInfo()[1] == 1062) {
+            if ($e instanceof PDOException && $stmtInsert->errorInfo()[0] == 23000 && $stmtInsert->errorInfo()[1] == 1062) {
                 if ($db != null) $db->rollback();
             } else {
                 throw $e;
